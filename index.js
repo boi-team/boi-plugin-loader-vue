f76f7b26c54ccdf6ccc3d5434316d39389906a10
zhoujunpeng <zjp0432@163.com>
2016-04-29 11:37:40 +0800

    add doc

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ let options = {
         }
     },
     noParse: null,
-    plugins: null
+    plugins: null,
+    // 插件依赖的module
+    // 此配置项是为了解决低版本npm树形安装node_modules引起的module寻址问题
+    // 如果你确定使用npm 3.0.0及以上版本，可以不配置此项
+    dependencies: [
+        'vue-loader'
+    ]
 };
 module.exports = new boli.PluginLoader('extend', options);
