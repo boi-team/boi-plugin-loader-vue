ac7ae00b18bcd2c03c47244f4ebbb7bbfe65c7ed
zhoujunpeng <zjp0432@163.com>
2016-05-04 16:41:59 +0800

    使用全局boi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let ClassLoader = require('boi-kernel').PluginClass.loader;
+let ClassLoader = boi.PluginClass.loader;
 
 let options = {
     module: {
@@ -14,11 +14,13 @@ let options = {
     },
     noParse: null,
     plugins: null,
-    // 插件依赖的module
+    // 插件依赖的第三方module
     // 此配置项是为了解决低版本npm树形安装node_modules引起的module寻址问题
     // 如果你确定使用npm 3.0.0及以上版本，可以不配置此项
     dependencies: [
         'vue-loader'
     ]
 };
-module.exports = new ClassLoader('extend', options);
+
+// 每个插件中创建实例的数量不限,但是建议每个loader插件只创建一个实例
+new ClassLoader('extend', options);
